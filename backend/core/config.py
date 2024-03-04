6869bcda72f199e68017a888e8dd6e3991d0b019
kylych-bob <mirbekov1kylych@gmail.com>
2024-03-04 19:46:32 +0600

    add celery

--- backend/core/config.py
+++ backend/core/config.py
@@ -1,20 +1,18 @@
 from fastapi import FastAPI, HTTPException, Query
 from pydantic import BaseModel
 
-from .db_connect import insert_repository, insert_repository2, insert_owner, insert_stars
+import db_connect as db
 from psycopg2 import sql
 import requests
 
 
 from datetime import datetime
 from typing import List
-from .utils import params, top_test
+from utils import params, top_test
 
 
 
 app = FastAPI()
-
-
 
 
 
@@ -27,73 +25,15 @@ def top_test_language(language: str, sort_by: str = 'stars'):
     # Получение списка репозиториев
     repositories = top_test(params_language)
 
+    # Запись репозиториев в базу данных
     for repo in repositories:
         if 'language' in repo:
             repo_data = (repo['language'], ) 
-            insert_repository(repo_data, 'language')
+            db.insert_repository(repo_data, 'language')
         else:
             print("Error: Missing 'language' key in repository data")
 
-
     return repositories
-
-
-# ________________________________________________________________
-
-# @app.get("/top_repo/")
-# def top_repo(owner: str = None, sort_by: str = 'stars'):
-#     params_repo = params.copy()
-#     query = 'is:public'
-#     if owner:
-#         query += f' user:{owner}'
-#     params_repo['q'] = query
-#     params_repo['sort'] = sort_by
-    
-#     # Получение списка репозиториев
-#     repositories = top_test(params_repo)
-
-#     # Запись репозиториев в базу данных
-#     for repo in repositories:
-#         if 'name' in repo and 'owner' in repo and 'stars' in repo and 'watchers' in repo and 'forks' in repo and 'open_issues' in repo and 'language' in repo:
-#             repo_data = (repo['name'], repo['owner'], repo['stars'], repo['watchers'], repo['forks'], repo['open_issues'], repo['language'])
-#             insert_repository(repo_data)
-#         else:
-#             print("Error: Missing key(s) in repository data")
-    
-#     return repositories
-# ________________________________________________________________
-
-
-
-# @app.get("/top_language/{language}")
-# def top_test_language(language: str, sort_by: str = 'stars'):
-#     params_language = params.copy()
-#     params_language['q'] = f'language:{language} is:public'
-#     params_language['sort'] = sort_by
-
-#     # Получение списка репозиториев и их форматирование
-#     repositories = top_test(params_language)
-
-#     # Здесь можно вставить данные в базу данных для каждого репозитория
-#     for repo in repositories:
-#         repo_data = (repo['name'], repo['owner'], repo['position_cur'], repo['position_prev'], repo['stars'], repo['watchers'], repo['forks'], repo['language'])
-#         insert_repository(repo_data)
-
-#     return repositories
-
-
-
-
-# ________________________________________________________________
-# @app.get("/top_language/{language}")
-# def top_test_language(language: str, sort_by: str = 'stars'):
-#     params_language = params.copy()
-#     params_language['q'] = f'language:{language} is:public'
-#     params_language['sort'] = sort_by
-#     return top_test(params_language)
-# ________________________________________________________________
-
-
 
 @app.get("/top_repo/")
 def top_repo(owner: str = None, sort_by: str = 'stars'):
@@ -103,21 +43,16 @@ def top_repo(owner: str = None, sort_by: str = 'stars'):
         query += f' user:{owner}'
     params_repo['q'] = query
     params_repo['sort'] = sort_by
-
+    
     # Получение списка репозиториев
     repositories = top_test(params_repo)
 
     # Запись репозиториев в базу данных
     for repo in repositories:
-        if 'language' in repo and 'forks' in repo:
-            repo_data = (repo['language'], repo['forks'])
-            insert_repository2(repo_data)
-        else:
-            print("Error: Missing 'language' or 'forks' key in repository data")
-
+        repo_data = (repo['name'], repo['stargazers_count'])  # Предположим, что здесь 'name' и 'stargazers_count' - это нужные данные
+        db.insert_repository(repo_data)
+    
     return repositories
-
-
 
 @app.get("/top_owner/")
 def top_owner(owner: str = None, sort_by: str = 'stars'):
@@ -127,124 +62,65 @@ def top_owner(owner: str = None, sort_by: str = 'stars'):
         query += f' user:{owner}'
     params_top['q'] = query
     params_top['sort'] = sort_by
-
-    # Получение списка репозиториев
-    repositories = top_test(params_top)
-
-    # Подсчет количества репозиториев для каждого владельца
-    owner_counts = {}
-    for repo in repositories:
-        owner = repo.get('owner', {}).get('login')  # Получаем имя владельца
-        owner_counts[owner] = owner_counts.get(owner, 0) + 1
-
-    # Запись данных в базу данных
-    for owner, count in owner_counts.items():
-        owner_data = (owner, count)
-        insert_owner(owner_data)
-
-    return owner_counts
-
-
-@app.get("/top_stars/")
-def top_stars():
-    params_top = params.copy()
-    params_top['sort'] = 'stars'
-
+    
     # Получение списка репозиториев
     repositories = top_test(params_top)
 
     # Запись данных в базу данных
     for repo in repositories:
-        repo_name = repo.get('name')
-        stars = repo.get('stargazers_count')
-        stars_data = (repo_name, stars)
-        insert_stars(stars_data)
-
+        owner_data = (repo['owner']['login'], )  # здесь 'owner' и 'login' - это нужные данные
+        db.insert_owner(owner_data)
+    
     return repositories
-
-
-# ________________________________________________________________
-
-
-
-
-
-
-
-'''
-
-@app.get("/top_repo/")
-def top_repo(owner: str = None, sort_by: str = 'stars'):
-    params_repo = params.copy()
-    query = 'is:public'
-    if owner:
-        query += f' user:{owner}'
-    params_repo['q'] = query
-    params_repo['sort'] = sort_by
-    return top_test(params_repo)
-
-@app.get("/top_owner/")
-def top_owner(owner: str = None, sort_by: str = 'stars'):
-    params_top = params.copy()
-    query = 'is:public'
-    if owner:
-        query += f' user:{owner}'
-    params_top['q'] = query
-    params_top['sort'] = sort_by
-    return top_test(params_top)
 
 @app.get("/top_stars")
 def top_stars():
     params['sort'] = 'stars'
-    return top_test(params)
+    repositories = top_test(params)
+
+    # Запись данных в базу данных
+    for repo in repositories:
+        stars_data = (repo['name'], repo['stargazers_count'])  # здесь 'name' и 'stargazers_count' - это нужные данные
+        db.insert_stars(stars_data)
+    
+    return repositories
 
 @app.get("/top_watchers")
 def top_watchers():
     params['sort'] = 'watchers'
-    return top_test(params)
+    repositories = top_test(params)
+
+    # Запись данных в базу данных
+    for repo in repositories:
+        watchers_data = (repo['name'], repo['watchers_count'])  # здесь 'name' и 'watchers_count' - это нужные данные
+        db.insert_watchers(watchers_data)
+    
+    return repositories
 
 @app.get("/top_forks")
 def top_forks():
     params['sort'] = 'forks'
-    return top_test(params)
+    repositories = top_test(params)
+
+    # Запись данных в базу данных
+    for repo in repositories:
+        forks_data = (repo['name'], repo['forks_count'])  # здесь 'name' и 'forks_count' - это нужные данные
+        db.insert_forks(forks_data)
+    
+    return repositories
 
 @app.get("/top_open_issues")
 def top_open_issues():
     params['sort'] = 'open_issues'
-    return top_test(params)
+    repositories = top_test(params)
 
-
-def get_commit_activity(owner, repo, since, until):
-    url = f"https://api.github.com/repos/{owner}/{repo}/commits"
-    params = {'since': since, 'until': until}
-    response = requests.get(url, params=params)
-
-    if response.status_code == 200:
-        commits = response.json()
-        commit_activity = {}
-        for commit in commits:
-            commit_date = commit['commit']['author']['date'][:10]  # extracting only date part
-            commit_activity[commit_date] = commit_activity.get(commit_date, 0) + 1
-        return commit_activity
-    else:
-        return None
-
-@app.get("/api/repos/{owner}/{repo}/activity")
-def repo_activity(owner: str, repo: str, since: str = None, until: str = None):
-    # Default to one year period if 'since' and 'until' are not provided
-    if since is None or until is None:
-        since = '2023-03-01T00:00:00Z'
-        until = '2024-03-01T00:00:00Z'
-        
-    # Call the function to get commit activity
-    commit_activity = get_commit_activity(owner, repo, since, until)
+    # Запись данных в базу данных
+    for repo in repositories:
+        open_issues_data = (repo['name'], repo['open_issues_count'])  # здесь 'name' и 'open_issues_count' - это нужные данные
+        db.insert_open_issues(open_issues_data)
     
-    if commit_activity:
-        return commit_activity
-    else:
-        return {"error": "Failed to fetch commit activity"}
+    return repositories
 
-'''
 
 '''
 owner = 'Kylych-dev'

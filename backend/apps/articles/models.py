73a2a7f286237466cf0dc0803cbc3adad4c93133
kylych-dev <mirbekov1kylych@gmail.com>
2025-02-20 23:21:37 +0600

    fix model

--- backend/apps/articles/models.py
+++ backend/apps/articles/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 
-from django.contrib.auth.models import PermissionsMixin
+from django.contrib.auth.models import PermissionsMixin, User
 from apps.users.models import CustomUser
 
 
@@ -9,9 +9,24 @@ class Article(models.Model):
     owner = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
     title = models.CharField(max_length=100)
     content = models.TextField()
+    likes = models.ManyToManyField(CustomUser, through='Like', related_name='liked_articles')
 
     date_created = models.DateTimeField(auto_now_add=True)
     date_updated = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.title
+
+
+class Like(models.Model):
+    article = models.ForeignKey(Article, on_delete=models.CASCADE)
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        unique_together = (
+            'user',
+            'article'
+        )
+
+

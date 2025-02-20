b02d3586cd5c3abd89a6c9b2b1ab2e910cb95615
kylych-dev <mirbekov1kylych@gmail.com>
2025-02-21 00:22:18 +0600

    prefetch related and select related

--- backend/api/v1/articles/views.py
+++ backend/api/v1/articles/views.py
@@ -1,3 +1,4 @@
+from django.db.models.aggregates import Count
 from rest_framework import permissions, viewsets
 from apps.articles.models import Article
 from api.common.permissions import IsOwnerOrReadOnly
@@ -6,10 +7,47 @@ from .serializers import ArticleSerializer
 
 
 class ArticleViewSet(viewsets.ModelViewSet):
-    queryset = Article.objects.all()
+    queryset = Article.objects.all().prefetch_related('likes')
     serializer_class = ArticleSerializer
     permission_classes = [permissions.IsAuthenticated, IsOwnerOrReadOnly]
 
+    def get_queryset(self):
+        queryset = super().get_queryset().select_related('owner').prefetch_related('likes')
+        # print(queryset, 'Article Queried -=-=-=-=')
+        queryset = queryset.prefetch_related('likes')
+        print(queryset.query)
+
+        return queryset
+
+    def list(self, request, *args, **kwargs):
+        response = super().list(request, *args, **kwargs)
+        print('Response data: ', response.data)
+        return response
+
+
+
+'''
+Article.objects.all()
+http://127.0.0.1:8000/api/v1/articles/
+|------|-----------|----------|----------|----------|------------|
+| Type | Database  |   Reads  |  Writes  |  Totals  | Duplicates |
+|------|-----------|----------|----------|----------|------------|
+| RESP |  default  |    6     |    0     |    6     |     5      |
+|------|-----------|----------|----------|----------|------------|
+
+http://127.0.0.1:8000/api/v1/articles/
+|------|-----------|----------|----------|----------|------------|
+| Type | Database  |   Reads  |  Writes  |  Totals  | Duplicates |
+|------|-----------|----------|----------|----------|------------|
+| RESP |  default  |    5     |    0     |    5     |     3      |
+|------|-----------|----------|----------|----------|------------|
+Total queries: 5 in 0.0234s 
+
+
+
+
+
+'''
 
     # def get_queryset(self):
     #     print(self.request.user, 'it is user')
